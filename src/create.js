3b51aeb6e94e2c639d2057b5aa5885ea96d8e9cc
-KaiK- <enriqueoriol@gmail.com>
2018-12-04 20:14:57 +0100

    done

--- src/create.js
+++ src/create.js
@@ -1,9 +1,13 @@
 import { displayLog } from './utils';
+import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
-
-
-
+    const hello = Observable.create(function(observer) {
+        observer.next('Hello');
+        observer.next('World');
+    });
+    
+    const subscribe = hello.subscribe(evt => displayLog(evt));
     /** end coding */
 }
