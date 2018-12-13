429f61bb58f9b60b4a139d67fa5a36cd33bb2e43
-KaiK- <enriqueoriol@gmail.com>
2018-12-13 13:18:06 +0100

    finished point 3

--- src/create.js
+++ src/create.js
@@ -1,7 +1,15 @@
 import { displayLog } from './utils';
+import { from } from 'rxjs';
 
 export default () => {
     /** start coding */
+    const myArray = [1,2,3,4,5];  
+    const myString = "Hello world!";  
+    const myPromise = new Promise(resolve => setTimeout( () => {
+        resolve('Hello World!'); 
+    },2000));
 
+    const observable = from(myPromise);
+    const subscription = observable.subscribe(val => displayLog(val));
     /** end coding */
 }
