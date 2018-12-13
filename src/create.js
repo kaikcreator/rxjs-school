91ff9a6fa23d1fde18abe61b54a7147e9baaf0a1
-KaiK- <enriqueoriol@gmail.com>
2018-12-13 12:41:33 +0100

    ready for point 3

--- src/create.js
+++ src/create.js
@@ -1,24 +1,7 @@
 import { displayLog } from './utils';
-import { Observable } from 'rxjs';
 
 export default () => {
     /** start coding */
-    const hello = Observable.create(function(observer) {
-        observer.next('Hello');
-        setTimeout(()=>{
-            observer.next('World');
-            observer.complete();
-        }, 2000);
-    });
-    
-    const observer = {
-        next: evt => displayLog(evt),
-        error: err => console.error("[ERR] - ", err),
-        complete: () => displayLog("[DONE]")
-    }
 
-    const subscribe = hello.subscribe(observer);
-    const subscribe2 = hello.subscribe(observer);
-    subscribe.unsubscribe();
     /** end coding */
 }
