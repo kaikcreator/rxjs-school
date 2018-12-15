8e896162ba02d1187b31d249249dd8628ff373fb
-KaiK- <enriqueoriol@gmail.com>
2018-12-15 01:47:59 +0100

    typo

--- src/intro-to-rxjs/example7.js
+++ src/intro-to-rxjs/example7.js
@@ -5,11 +5,11 @@ import { take } from 'rxjs/operators';
 export default () =>{
     /** example code here */
 
-    function my5sInterval(ms){
+    function my5Interval(ms){
         return Observable.create((observer) =>{
             let count = 0;
             const id = setInterval(()=>{
-                observer.next(count);
+                observer.next("value1");
                 count++;
                 if(count >=5 ){ 
                     clearInterval(id); 
@@ -19,13 +19,18 @@ export default () =>{
         });
     }
 
-    const observable = my5sInterval(1000);
+    const observable = my5Interval(1000);
 
     const observer = {
         next: x => console.log(x),
         error: err => console.error(err),
         complete: () => console.log('done')
     };
+
+    observable.subscribe({
+        next: value => console.log(value),
+        complete: () => console.log('done')
+    });
 
     observable.subscribe(observer);
     //output: 0, 1, 2, 3, done
