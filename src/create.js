ab8eea02e4397b4c290fe30b152cccd832f4742b
-KaiK- <enriqueoriol@gmail.com>
2018-12-17 13:31:56 +0100

    point 4 done

--- src/create.js
+++ src/create.js
@@ -1,8 +1,19 @@
 import { displayLog } from './utils';
+import { of, range } from 'rxjs';
 
 export default () => {
     /** start coding */
 
+    const source = of(1, 2, 3, 4, 5, 6);
+    const source2 = of(
+        [1, 2, 3], 
+        "hello world", 
+        { foo: 'bar' }, 
+        function sayHello() {
+        return 'Hi!';
+      });
+    const source3 = range(3,10);
+    const subscription = source3.subscribe(data => displayLog(data));
 
 
     /** end coding */
