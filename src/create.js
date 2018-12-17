9fa69c008f33f16b6ab04d70a33c94624adb2624
-KaiK- <enriqueoriol@gmail.com>
2018-12-17 14:35:36 +0100

    point 5 completed

--- src/create.js
+++ src/create.js
@@ -1,7 +1,20 @@
 import { displayLog } from './utils';
+import { interval, timer } from 'rxjs';
+
 
 export default () => {
     /** start coding */
+    
+    const source = interval(500);
+    
+    const subscription = source.subscribe(data => displayLog(data));
+
+    timer(3000).subscribe(()=> subscription.unsubscribe());
+    //setTimeout(() => subscription.unsubscribe(), 3000 );
+
+    const source2 = timer(4000, 100);
+    const subscription2 = source2.subscribe(data => displayLog(`2 - ${data}`));
+    timer(6000).subscribe(() => subscription2.unsubscribe());
 
 
 
