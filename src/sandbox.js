226f232590112fb0dbe90066c88b306fb3dc9405
-KaiK- <enriqueoriol@gmail.com>
2019-01-02 19:23:53 +0100

    completed point 8

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,15 +1,17 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const click$ = fromEvent(grid, 'click').pipe(
+        tap(val => console.log('before: ', val)),
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ])
+        ]),
+        tap(val => console.log('after: ', val)),
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
