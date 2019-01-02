f772e80b94652962c9a5c1e3489f1725d03a914a
-KaiK- <enriqueoriol@gmail.com>
2019-01-02 17:06:18 +0100

    updated point 8

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -9,8 +9,7 @@ export default () => {
         map(val => [ 
             Math.floor(val.offsetX/50), 
             Math.floor(val.offsetY/50)
-        ]),
-        filter( val => (val[0] + val[1]) % 2 != 0 )
+        ])
     );
 
     const subscription = click$.subscribe(data => displayLog(data));
