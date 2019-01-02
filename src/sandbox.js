2526ac695daefe339afe72386ad0827a290f1854
-KaiK- <enriqueoriol@gmail.com>
2019-01-02 17:04:35 +0100

    completed point 7

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,19 @@
 import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
+    const click$ = fromEvent(grid, 'click').pipe(
+        map(val => [ 
+            Math.floor(val.offsetX/50), 
+            Math.floor(val.offsetY/50)
+        ]),
+        filter( val => (val[0] + val[1]) % 2 != 0 )
+    );
 
+    const subscription = click$.subscribe(data => displayLog(data));
 
     /** end coding */
 }
