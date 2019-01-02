ce8569f74f2b82095cd49b81357cf1ec539c9ebc
-KaiK- <enriqueoriol@gmail.com>
2019-01-02 14:49:01 +0100

    updated dev/07

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //import stuff
-import example from './create';
+import example from './sandbox';
 
 //run stuff
 example();
---
+++ src/sandbox.js
@@ -0,0 +1,9 @@
+import { displayLog } from './utils';
+
+export default () => {
+    /** start coding */
+    const grid = document.getElementById('grid');
+
+
+    /** end coding */
+}
