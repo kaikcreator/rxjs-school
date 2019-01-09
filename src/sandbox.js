f96156575c0887956e9146dd51dcace4fcf051a7
-KaiK- <enriqueoriol@gmail.com>
2019-01-09 11:04:29 +0100

    work in progress

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,19 +1,40 @@
 import { displayLog } from './utils';
-import { fromEvent } from 'rxjs';
-import { map, /*first, take,*/ takeWhile } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { map, /*first, take,*/ takeWhile, mapTo, startWith, switchMap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
+    const gridSize = 3;
+    const cellWidth = 50;
+
+    //empty grid
+    const gameState = {
+        board: Array(gridSize).fill().map(() => Array(gridSize).fill(0)),
+        nextPlayer: 1 //1 or 2
+    }
+
+    //user selection
     const click$ = fromEvent(grid, 'click').pipe(
-        map(val => [ 
-            Math.floor(val.offsetX/50), 
-            Math.floor(val.offsetY/50)
-        ]),
-        // first(val => val[0]>3)
-        // take(4)
-        takeWhile( ([col, row]) => col > 3 )
+        map(val =>  { return {
+            nextPlayer: 'user',
+            col: Math.floor(val.offsetX/cellWidth), 
+            row: Math.floor(val.offsetY/cellWidth)
+        }})
     );
+
+    //computer selection
+    const computer$ = interval(1000).pipe(
+        //return random cell
+        map( val => [Math.floor(Math.random()*3)][Math.floor(Math.random()*3)] )
+    )
+
+    //
+    const game$ = merge(reducer1, reducer2, ...).pipe(
+        startWith(gameState),
+        switchMap(({nextPlayer}) => nextPlayer == 1 ? click$ : computer$),
+        scan((acc/* acc is the state */,reducer) => reducer(acc))
+    )
 
     const subscription = click$.subscribe(data => displayLog(data));
 
