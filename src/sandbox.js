f7034007fdef88bc613ae6f7d9220236937f8453
-KaiK- <enriqueoriol@gmail.com>
2019-01-09 19:38:21 +0100

    completed point 14

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, pairwise } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -13,7 +13,12 @@ export default () => {
     //observable that returns scroll (from top) on scroll events
     const scroll$ = fromEvent(document, 'scroll').pipe(
         map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
+        tap(evt => console.log("[scroll]: ", evt)),
+        pairwise(),
+        tap(([previous, current]) =>{
+            updateDisplay(current > previous ? 'DESC' : 'ASC');
+        }),
+        map(([previous, current]) => current)
     );
 
     //observable that returns the amount of page scroll progress
