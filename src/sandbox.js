3f0c20b18641fb62c8e9c8eb620cc484c94c39c6
-KaiK- <enriqueoriol@gmail.com>
2019-01-23 12:12:11 +0100

    completed point 19

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,9 +1,18 @@
 import { updateDisplay, displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     
     const inputBox = document.getElementById('input-box');
+
+    const inputSrc$ = fromEvent(inputBox, "input").pipe(
+        debounceTime(300),
+        map(event => event.target.value)
+    );
+
+    inputSrc$.subscribe(displayLog);
 
     /** end coding */
 }
