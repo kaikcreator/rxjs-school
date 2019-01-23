d6f25c4438c551b8b8222e8160fbf48109685078
-KaiK- <enriqueoriol@gmail.com>
2019-01-23 11:23:18 +0100

    ready to start point 19

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,38 +1,9 @@
-import { updateDisplay } from './utils';
-import { fromEvent} from 'rxjs';
-import { map, tap, delay, bufferTime } from 'rxjs/operators';
+import { updateDisplay, displayLog } from './utils';
 
 export default () => {
     /** start coding */
     
-    const progressBar = document.getElementById('progress-bar');
-    const docElement = document.documentElement;
-
-    //function to update progress bar width on view
-    const updateProgressBar = (percentage) => {
-        progressBar.style.width = `${percentage}%`;
-    }
-
-    //observable that returns scroll (from top) on scroll events
-    const scroll$ = fromEvent(document, 'scroll').pipe(
-        map(() => docElement.scrollTop),
-        tap(evt => console.log("[scroll]: ", evt))
-    );
-
-    //observable that returns the amount of page scroll progress
-    const scrollProgress$ = scroll$.pipe(
-        map(evt => {
-            const docHeight = docElement.scrollHeight - docElement.clientHeight;
-            return (evt / docHeight) * 100;
-        }),
-        //delay(500)
-        bufferTime(300, 1000),
-        tap(evt => console.log("[buffer]: ", evt))
-    )
-
-    //subscribe to scroll progress to paint a progress bar
-    const subscription = scrollProgress$.subscribe(updateProgressBar);
-
+    const inputBox = document.getElementById('input-box');
 
     /** end coding */
 }
