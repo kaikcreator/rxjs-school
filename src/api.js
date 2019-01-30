515c23c2ea3452f739a6bb04ff5add07659128d0
-KaiK- <enriqueoriol@gmail.com>
2019-01-30 16:38:28 +0100

    ready to start point 25

--- src/api.js
+++ src/api.js
@@ -7,4 +7,18 @@ export class api{
             mapTo({id:id, comment:`comment number ${id}`})
         );
     }
+
+    static getCommentsList(page){
+        const buildCommentsList = (page) =>{
+            let comments = [];
+            const offset = (page-1)*10;
+            for(let i=offset; i < offset+10; i++){
+                comments.push({id:i, comment:`comment number ${i}`})
+            }
+            return comments;
+        }
+        return timer(Math.random()*1000).pipe(
+            mapTo(buildCommentsList(page))
+        );
+    }    
 }
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,7 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
 import { fromEvent } from 'rxjs';
-import { map, scan, tap, mergeMap, switchMap, concatMap } from 'rxjs/operators';
+import { map, scan, tap, concatMap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -11,8 +11,7 @@ export default () => {
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
         scan((acc, evt) => acc + 1, 0),            
-        //switchMap(id => api.getComment(id)),
-        concatMap(id => api.getComment(id)),
+        concatMap(page => api.getCommentsList(page)),
         map(JSON.stringify),
         tap(console.log),
     ).subscribe(displayLog);
