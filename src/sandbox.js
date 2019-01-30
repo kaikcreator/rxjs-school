d2c4494b0b57fea2ea45dfa488d5843eb28eaa6a
-KaiK- <enriqueoriol@gmail.com>
2019-01-30 11:55:19 +0100

    ready to start point 24

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,39 +1,18 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
-import { concat, fromEvent } from 'rxjs';
-import { map, endWith, tap, mergeAll, mergeMap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { map, scan, tap, mergeMap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
     
     const button = document.getElementById('btn');
 
-    /** get 4 consecutive comments */
-    const getComments = () =>{
-        //get observables from fake REST API.
-        const comment1$ = api.getComment(1);
-        const comment2$ = api.getComment(2);
-        const comment3$ = api.getComment(3);
-        const comment4$ = api.getComment(4);
-
-        //subscribe to all the observables to get and display comments
-        concat(comment1$, comment2$, comment3$, comment4$).pipe(
-            map(JSON.stringify),
-            endWith('--------//--------')
-        ).subscribe(data =>{
-            displayLog(data);
-        })
-    }
-
-    const observable2$ = api.getComment(1).pipe(
-        map(JSON.stringify)
-    );
-
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
-        // map(evt => observable2$),
-        // mergeAll(),        
-        mergeMap(evt => observable2$),
+        scan((acc, evt) => acc + 1, 0),            
+        mergeMap(id => api.getComment(id)),
+        map(JSON.stringify),
         tap(console.log),
     ).subscribe(displayLog);
 
