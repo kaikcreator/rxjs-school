ef572d0447ca0ec9be1f6cb7f0cf2c48f9566c9f
-KaiK- <enriqueoriol@gmail.com>
2019-02-01 00:16:19 +0100

    completed point 26

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
-import { timer } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { timer, throwError, of } from 'rxjs';
+import { mapTo, mergeMap } from 'rxjs/operators';
 
 export class api{
     static getComment(id){
         return timer(Math.random()*1000).pipe(
-            mapTo({id:id, comment:`comment number ${id}`})
+            mergeMap(evt =>{
+                const isError = Math.random() > 0.1 ? true : false;
+                if(isError){
+                    return throwError(new Error('Request Timeout'));
+                }
+                else{
+                    return of({id:id, comment:`comment number ${id}`})
+                }
+            })
         );
     }
 
--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,7 @@
 import { updateDisplay, displayLog } from './utils';
 import { api } from './api';
-import { fromEvent } from 'rxjs';
-import { map, scan, tap, concatMap } from 'rxjs/operators';
+import { fromEvent, of, empty } from 'rxjs';
+import { map, scan, tap, concatMap, catchError, retry } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
@@ -11,10 +11,13 @@ export default () => {
     /** get comments on button click */
     fromEvent(button, 'click').pipe(
         scan((acc, evt) => acc + 1, 0),            
-        concatMap(id => api.getComment(id)),
+        concatMap(id => api.getComment(id).pipe(
+            retry(3),
+            //catchError((err, src$) => { console.log("catch!"); return src$; })
+            )),
         map(JSON.stringify),
         tap(console.log),
-    ).subscribe(displayLog);
+    ).subscribe(displayLog, err => {console.log("Error detected - ", err.message)});
 
     /** end coding */
 }
