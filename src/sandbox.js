78c9260965d6b87a586155099349ed59404b7d76
-KaiK- <enriqueoriol@gmail.com>
2019-02-04 23:54:33 +0100

    added timer on finish game feedback

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,7 @@
 import { displayLog } from './utils';
 import { game$ } from './tic-tac-toe/game';
 import { drawGame, writeMessage } from './tic-tac-toe/draw'; 
+import { timer } from 'rxjs';
 
 
 export default () => {
@@ -9,10 +10,11 @@ export default () => {
     const subscription = game$.subscribe(gameState => {
         drawGame(gameState);
         if(gameState.winner){
-            writeMessage(gameState.winner == 1 ? 'you win' : 'you lose');
+            timer(500).subscribe(()=>writeMessage(gameState.winner == 1 ? 'you win' : 'you lose'));
+            
         }
         else if(gameState.finished){
-            writeMessage("draws");
+            timer(500).subscribe(()=>writeMessage('draws'));
         }
     }, 
     err => console.log("error: ", err), data => console.log("COMPLETE"));
