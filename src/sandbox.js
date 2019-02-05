233a04e99acd6983d6ad81e488e2bd07e39f2ceb
-KaiK- <enriqueoriol@gmail.com>
2019-02-05 11:17:38 +0100

    ready to start point 27

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,23 +1,33 @@
-import { updateDisplay, displayLog } from './utils';
-import { api } from './api';
-import { fromEvent, of, empty } from 'rxjs';
-import { map, scan, tap, concatMap, catchError, retry } from 'rxjs/operators';
+import { updateDisplay } from './utils';
+import { fromEvent, interval } from 'rxjs';
+import { mapTo, scan, takeWhile  } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
+
+    /** number of seconds to init countdown */
+    const countdownSeconds = 10;
     
-    const button = document.getElementById('btn');
+    /** access interface buttons */
+    const pauseButton = document.getElementById('pause-btn');
+    const resumeButton = document.getElementById('resume-btn');
 
     /** get comments on button click */
-    fromEvent(button, 'click').pipe(
-        scan((acc, evt) => acc + 1, 0),            
-        concatMap(id => api.getComment(id).pipe(
-            retry(3),
-            //catchError((err, src$) => { console.log("catch!"); return src$; })
-            )),
-        map(JSON.stringify),
-        tap(console.log),
-    ).subscribe(displayLog, err => {console.log("Error detected - ", err.message)});
+    const pause$ = fromEvent(pauseButton, 'click');
+    const resume$ = fromEvent(resumeButton, 'click');
 
+    /** 1s negative interval */
+    const interval$ = interval(1000).pipe(mapTo(-1));
+
+    /** countdown timer */
+    const countdown$ = interval$.pipe(
+        scan((acc, curr) => ( curr ? curr + acc : curr ), countdownSeconds),
+        takeWhile(v => v >= 0)
+    );
+
+    /** subscribe to countdown */
+    countdown$.subscribe(updateDisplay);
+
+    
     /** end coding */
 }
