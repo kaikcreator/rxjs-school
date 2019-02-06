3bd79af223fed6d15c7e416d23811dcdb22c7f30
-KaiK- <enriqueoriol@gmail.com>
2019-02-06 23:54:47 +0100

    updated point 29 with finish game feedback message

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,6 @@
 import { game$ } from './tic-tac-toe/game';
 import { drawGame, writeMessage } from './tic-tac-toe/draw'; 
+import { timer } from 'rxjs';
 
 
 export default () => {
@@ -7,6 +8,11 @@ export default () => {
     
     game$.subscribe(gameState => {
         drawGame(gameState);
+        if(gameState.winner){
+            timer(500).subscribe(()=>writeMessage(gameState.winner == 1 ? 'you win' : 'you lose'));
+        }else if(gameState.finished){
+            timer(500).subscribe(()=>writeMessage('draws'));
+        }
     }, 
     err => console.log("error: ", err), 
     data => console.log("COMPLETE"));
